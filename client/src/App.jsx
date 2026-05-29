0d367f7a408fa04c5d470d9449415bceb462c553
devrohitraj838 <raj826444@gmail.com>
2026-05-30 00:07:25 +0530

    Day 1 progress

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      
+<p>Building my first MERN + AI project 🚀</p>
+
+<h3>Day 1 Progress</h3>
+<p>✅ React Setup</p>
+<p>✅ Project Structure</p>
+<p>🔄 Learning Components</p>
+    </div>
+  );
+}
+
+export default App;
