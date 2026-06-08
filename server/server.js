8964e76ead2054472b8b91b9bcd35556fd246eb9
devrohitraj838 <raj826444@gmail.com>
2026-06-08 15:35:51 +0530

    create expenses API returning JSON data

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("BudgetTracker Backend Running");
 });
 
+app.get("/expenses", (req, res) => {
+  const expenses = [
+    { id: 1, title: "Pizza", amount: 300 },
+    { id: 2, title: "Movie", amount: 500 },
+  ];
+
+  res.json(expenses);
+});
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
