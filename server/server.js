7a3a25e28f7064fdb79a751bbb7d693fe17e1078
devrohitraj838 <raj826444@gmail.com>
2026-06-09 21:36:20 +0530

    add get expenses by id API

--- server/server.js
+++ server/server.js
@@ -6,13 +6,19 @@ app.get("/", (req, res) => {
   res.send("BudgetTracker Backend Running");
 });
 
-app.get("/expenses", (req, res) => {
+app.get("/expenses/:id", (req, res) => {
   const expenses = [
     { id: 1, title: "Pizza", amount: 300 },
     { id: 2, title: "Movie", amount: 500 },
   ];
 
-  res.json(expenses);
+  const expenseId = Number(req.params.id);
+
+  const expense = expenses.find(
+    item => item.id === expenseId
+  );
+
+  res.json(expense);
 });
 
 app.listen(5000, () => {
