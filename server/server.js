e665247684a954be6f689fd6f48f27db005327d6
devrohitraj838 <raj826444@gmail.com>
2026-06-12 12:06:59 +0530

    Add validation and status codes to create expense API

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("BudgetTracker Backend Running");
 });
+app.post("/expenses", (req, res) => {
+  const { title, amount } = req.body;
 
+  if (!title || !amount) {
+    return res.status(400).json({
+      message: "Title and amount are required",
+    });
+  }
+
+  res.status(201).json(req.body);
+});
 app.get("/expenses/:id", (req, res) => {
   const expenses = [
     { id: 1, title: "Pizza", amount: 300 },
     { id: 2, title: "Movie", amount: 500 },
   ];
+
 
   const expenseId = Number(req.params.id);
 
